9cfcdcc1a203e4a31cbfa9e1b68439f4062d9988
Akbar <akbarasghari@outlook.com>
2025-03-20 16:20:39 +0330

    Add SEO Provider for Search Engins

---
+++ DNSLab.Web/Components/Custom/SeoProvider.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DNSLab.Web.Components.Custom;
+
+partial class SeoProvider
+{
+    [Parameter] public string Title { get; set; }
+    [Parameter] public string Meta_Description { get; set; }
+}

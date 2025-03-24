14c0d90a5b28eaa61b4637faa644845cecbdf815
Akbar <akbarasghari@outlook.com>
2025-03-24 16:46:58 +0330

    Remove Extra Page in Monitoring

--- DNSLab.Web/Interfaces/Repositories/IDNSLogRepository.cs
+++ DNSLab.Web/Interfaces/Repositories/IDNSLogRepository.cs
@@ -5,10 +5,7 @@ namespace DNSLab.Web.Interfaces.Repositories
 {
     public interface IDNSLogRepository
     {
-        Task<IEnumerable<QueryLogDTO>?> GetLastQueries(Guid zoneId);
-        Task<IEnumerable<QueryCountDTO>?> GetQueriesChart(Guid zoneId);
         Task<long?> GetTotalRequest(string qName);
         Task<IEnumerable<TimeAndCountDTO>?> GetTotalRequestChartByTime(string qName);
-        Task<IEnumerable<QueryCountDTO>?> GetQueriesByRecordChart(Guid recordId , RecordTypeEnum recordType);
     }
 }
--- DNSLab.Web/Repositories/DNSLogRepository.cs
+++ DNSLab.Web/Repositories/DNSLogRepository.cs
@@ -8,21 +8,7 @@ namespace DNSLab.Web.Repositories
     public class DNSLogRepository(IHttpServiceProvider _HttpServiceProvider) : IDNSLogRepository
     {
         const string APIController = "DNSLog";
-        public async Task<IEnumerable<QueryLogDTO>?> GetLastQueries(Guid zoneId)
-        {
-            return await _HttpServiceProvider.Get<IEnumerable<QueryLogDTO>?>($"{APIController}/GetLastQueries?zoneId={zoneId}");
-        }
-
-        public async Task<IEnumerable<QueryCountDTO>?> GetQueriesChart(Guid zoneId)
-        {
-            return await _HttpServiceProvider.Get<IEnumerable<QueryCountDTO>?>($"{APIController}/GetQueriesChart?zoneId={zoneId}");
-        }
-
-        public async Task<IEnumerable<QueryCountDTO>?> GetQueriesByRecordChart(Guid recordId, RecordTypeEnum recordType)
-        {
-            return await _HttpServiceProvider.Get<IEnumerable<QueryCountDTO>?>($"{APIController}/GetQueriesByRecordChart?recordId={recordId}&recordType={(int)recordType}");
-        }
-
+    
         public async Task<long?> GetTotalRequest(string qName)
         {
             return await _HttpServiceProvider.Get<long?>($"{APIController}/GetTotalRequest?qName={qName}");

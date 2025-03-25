8e887e8edc7504a74adbb83b722ee9c60c3ed700
Akbar <akbarasghari@outlook.com>
2025-03-25 18:04:52 +0330

    Add Spam Check Message in mails

--- DNSLab.Web/AppSettings.cs
+++ DNSLab.Web/AppSettings.cs
@@ -6,7 +6,7 @@ public sealed class GlobalSettings
 {
     public const string ApplicationName = "DNSLab";
     public const bool RightToLeft = true;
-    public const string Version = "1.0.21.3";
+    public const string Version = "1.1.3.2";
 
 #if DEBUG
     public const string APIBaseAddress = "https://localhost:7046/";

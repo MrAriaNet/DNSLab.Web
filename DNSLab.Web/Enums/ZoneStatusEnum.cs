2b068347d757a340d7327c78b962c8c61aa5a04e
Akbar <akbarasghari@outlook.com>
2025-05-15 00:42:18 +0330

    Add Zone Status

---
+++ DNSLab.Web/Enums/ZoneStatusEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNSLab.Web.Enums
+{
+    public enum ZoneStatusEnum
+    {
+        Pending = 1,
+        Active = 2,
+        Warning = 3,
+        Suspended = 4,
+        Expired = 5,
+        Deleted = 6,
+    }
+}

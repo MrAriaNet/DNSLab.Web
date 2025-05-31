0574685d541b0769f967834198cd1f86916232e7
Akbar <akbarasghari@outlook.com>
2025-06-01 01:00:10 +0330

    Add Raspberry Pi Settings Page

---
+++ DNSLab.Web/Components/Pages/DynamicDNSs/RaspberryPi.razor.cs
@@ -0,0 +1,32 @@
+﻿using DNSLab.Web.DTOs.Repositories.DDNS;
+using DNSLab.Web.Interfaces.Repositories;
+using Microsoft.AspNetCore.Components;
+
+namespace DNSLab.Web.Components.Pages.DynamicDNSs;
+
+public partial class RaspberryPi
+{
+    [Inject] IDDNSRepository _DDNSRepository { get; set; }
+    IEnumerable<UpdateTokenDTO>? _Tokens { get; set; }
+
+    bool _IsLoading = false;
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            _IsLoading = true;
+
+            _Tokens = await _DDNSRepository.GetTokens();
+
+            _IsLoading = false;
+            await InvokeAsync(() => StateHasChanged());
+        }
+    }
+
+
+    UpdateTokenDTO? _Token { get; set; } = null;
+    void SelectToken(UpdateTokenDTO token)
+    {
+        _Token = token;
+    }
+}

1c60ae9bac3299a0a38b8d1d7a50227e50eb23de
Akbar <akbarasghari@outlook.com>
2025-08-22 06:00:44 +0330

    Add History To Feature

--- DNSLab.Web/Components/Pages/Records/RecordChangesHistory.razor.cs
+++ DNSLab.Web/Components/Pages/Records/RecordChangesHistory.razor.cs
@@ -1,5 +1,6 @@
 ﻿using DNSLab.Web.DTOs.Repositories.Record;
 using DNSLab.Web.Interfaces.Repositories;
+using DNSLab.Web.Repositories;
 using Microsoft.AspNetCore.Components;
 
 namespace DNSLab.Web.Components.Pages.Records;
@@ -7,15 +8,22 @@ namespace DNSLab.Web.Components.Pages.Records;
 partial class RecordChangesHistory
 {
     [Inject] IRecordRepository _RecordRepository { get; set; }
+    [Inject] ISubscriptionRepository _SubscriptionRepository { get; set; }
 
     [Parameter] public Guid RecordId { get; set; }
 
-    IEnumerable<RecordChangeDTO>? _RecordChanges {  get; set; }
+    IEnumerable<RecordChangeDTO>? _RecordChanges { get; set; }
+    bool? _IsSubscribeThisFeature { get; set; } = null;
 
     bool _Loading = true;
     protected override async Task OnInitializedAsync()
     {
-        _RecordChanges = await _RecordRepository.GetRecordChangesHistory(RecordId);
+        _IsSubscribeThisFeature = await _SubscriptionRepository.CheckSbscriptionFeature(Enums.FeatureEnum.RecordChangesHistory);
+
+        if (_IsSubscribeThisFeature is not null && _IsSubscribeThisFeature == true)
+        {
+            _RecordChanges = await _RecordRepository.GetRecordChangesHistory(RecordId);
+        }
         _Loading = false;
     }
 }

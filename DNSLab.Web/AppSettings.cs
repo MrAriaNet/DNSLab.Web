eb5abc17bd8086c87359abf7a97ec2ad5844895b
Akbar <mrakbarasghari@gmail.com>
2025-09-30 02:20:27 +0330

    If Default NS was empty dnslab ns shows

--- DNSLab.Web/AppSettings.cs
+++ DNSLab.Web/AppSettings.cs
@@ -6,7 +6,7 @@ public sealed class GlobalSettings
 {
     public const string ApplicationName = "DNSLab";
     public const bool RightToLeft = true;
-    public const string Version = "1.0.11.8";
+    public const string Version = "1.0.12.0";
 
 #if DEBUG
     //public const string APIBaseAddress = "https://localhost:7046/";

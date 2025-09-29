98511acecedacb3f07a2606b15e06018697d646f
Akbar <mrakbarasghari@gmail.com>
2025-09-30 02:20:31 +0330

    Update UI for Monitoring

--- DNSLab.Web/AppSettings.cs
+++ DNSLab.Web/AppSettings.cs
@@ -6,7 +6,7 @@ public sealed class GlobalSettings
 {
     public const string ApplicationName = "DNSLab";
     public const bool RightToLeft = true;
-    public const string Version = "1.1.5.1";
+    public const string Version = "1.1.5.3";
 
 #if DEBUG
     //public const string APIBaseAddress = "https://localhost:7046/";

44d6b616bc066748145583a4971dc538ea2fef56
Akbar <mrakbarasghari@gmail.com>
2025-09-30 02:20:28 +0330

    Add Get Subscriptions

---
+++ DNSLab.Web/DTOs/Repositories/Subscription/SubscriptionDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace DNSLab.Web.DTOs.Repositories.Subscription
+{
+    public class SubscriptionDTO
+    {
+        public Guid Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public long TotalPrice { get; set; }
+
+        public UserInfoDTO? User { get; set; }
+        public PlanDTO Plan { get; set; }
+        public PlanDurationDTO Duration { get; set; }
+    }
+}

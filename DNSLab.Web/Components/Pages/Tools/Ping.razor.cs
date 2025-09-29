688a3941ff5dcac2d69db5626fb593192ee9106a
Akbar <mrakbarasghari@gmail.com>
2025-09-30 02:20:33 +0330

    Add Ping Tool

---
+++ DNSLab.Web/Components/Pages/Tools/Ping.razor.cs
@@ -0,0 +1,17 @@
+﻿using DNSLab.Web.Interfaces.Repositories;
+using Microsoft.AspNetCore.Components;
+
+namespace DNSLab.Web.Components.Pages.Tools;
+
+partial class Ping
+{
+    [Inject] IToolRepository _ToolRepository { get; set; }
+
+    string _Host;
+
+    string? _Result { get; set; }
+    async Task CalcPing()
+    {
+        _Result = await _ToolRepository.Ping(_Host);
+    }
+}

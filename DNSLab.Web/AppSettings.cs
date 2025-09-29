789ae4ee2d96f632d3ca7701705f72f4669e0517
Akbar <mrakbarasghari@gmail.com>
2025-09-30 02:20:26 +0330

    Add 404 NotFound Page

--- DNSLab.Web/AppSettings.cs
+++ DNSLab.Web/AppSettings.cs
@@ -6,7 +6,7 @@ public sealed class GlobalSettings
 {
     public const string ApplicationName = "DNSLab";
     public const bool RightToLeft = true;
-    public const string Version = "1.0.10.2";
+    public const string Version = "1.0.11.0";
 
 #if DEBUG
     //public const string APIBaseAddress = "https://localhost:7046/";

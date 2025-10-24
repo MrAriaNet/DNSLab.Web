12ccb3cb4978ace9608d99910a278b6154ca0ebb
Akbar <mrakbarasghari@gmail.com>
2025-10-25 01:44:11 +0330

    add feature description

--- DNSLab.Web/DTOs/Repositories/Bundle/FeatureSectionDTO.cs
+++ DNSLab.Web/DTOs/Repositories/Bundle/FeatureSectionDTO.cs
@@ -10,6 +10,7 @@ namespace DNSLab.Web.DTOs.Repositories.Bundle
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string? Description { get; set; }
 
         public List<FeatureDTO> Features { get; set; }
     }

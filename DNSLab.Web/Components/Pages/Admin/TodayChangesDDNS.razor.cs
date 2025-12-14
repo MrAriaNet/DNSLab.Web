1a9fd259a8ba707b8224291f6dec4de508a7c049
Akbar <mrakbarasghari@gmail.com>
2025-12-15 02:24:27 +0330

    Refactor TodayChangesDDNS page

--- DNSLab.Web/Components/Pages/Admin/TodayChangesDDNS.razor.cs
+++ DNSLab.Web/Components/Pages/Admin/TodayChangesDDNS.razor.cs
@@ -5,21 +5,39 @@ using DNSLab.Web.Interfaces.Repositories;
 using DNSLab.Web.Repositories;
 using Microsoft.AspNetCore.Components;
 using DNSLab.Web.DTOs.Repositories.Zone;
+using static MudBlazor.CategoryTypes;
+using System.Net.Http;
 
 namespace DNSLab.Web.Components.Pages.Admin
 {
     partial class TodayChangesDDNS
     {
         [Inject] IDDNSRepository _DDNSRepository { get; set; }
-        [Inject] IRecordRepository _RecordRepository { get; set; }
-        [Inject] IDialogService _DialogService { get; set; }
-        [Inject] ISnackbar _Snackbar { get; set; }
 
-        IEnumerable<Tuple<ZoneDTO, IEnumerable<BaseRecordDTO>>>? _AllRecords { get; set; }
-
-        protected override async Task OnInitializedAsync()
+        private async Task<GridData<BaseRecordDTO>> ServerReload(GridState<BaseRecordDTO> state)
         {
-            _AllRecords = await _DDNSRepository.GetTodayChangesDDNSDomainAndRecords();
+            IEnumerable<Tuple<ZoneDTO, IEnumerable<BaseRecordDTO>>>? data = await _DDNSRepository.GetTodayChangesDDNSDomainAndRecords();
+
+            if (data is null)
+            {
+                return new GridData<BaseRecordDTO>();
+            }
+
+            var totalItems = data.SelectMany(x => x.Item2).Count();
+            data.ToList().ForEach(x =>
+            {
+                x.Item2.ToList().ForEach(y =>
+                {
+                    y.Name = $"{y.Name}.{x.Item1.Name}";
+                });
+            });
+            var pagedData = data.SelectMany(x => x.Item2).Skip(state.Page * state.PageSize).Take(state.PageSize).ToArray();
+
+            return new GridData<BaseRecordDTO>
+            {
+                TotalItems = totalItems,
+                Items = pagedData.AsEnumerable()
+            };
         }
     }
 }

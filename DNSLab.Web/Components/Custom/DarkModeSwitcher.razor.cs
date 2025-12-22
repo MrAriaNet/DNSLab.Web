2d6ab8bd5afd70ee884188b5f57d33bf8de62a18
Akbar <mrakbarasghari@gmail.com>
2025-12-22 19:51:38 +0330

    Add DarkModeSwitch Component

---
+++ DNSLab.Web/Components/Custom/DarkModeSwitcher.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace DNSLab.Web.Components.Custom;
+
+partial class DarkModeSwitcher
+{
+    [Inject] public ProtectedLocalStorage _LocalStorage { get; set; }
+    [Parameter] public bool IsDarkModeToggle { get; set; }
+    [Parameter] public EventCallback<bool> IsDarkModeToggleChanged { get; set; }
+
+    private async Task HandleToggleChanged(bool newValue)
+    {
+        await _LocalStorage.SetAsync("ThemeMode", newValue);
+        IsDarkModeToggle = newValue;
+        await IsDarkModeToggleChanged.InvokeAsync(newValue);
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            var data = await _LocalStorage.GetAsync<bool>("ThemeMode");
+            if (data.Success)
+            {
+                await HandleToggleChanged(data.Value);
+                IsDarkModeToggle = data.Value;
+            }
+        }
+    }
+}

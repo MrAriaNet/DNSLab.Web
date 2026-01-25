894a606717169d4c6851e25f27b048b3d6cedfb5
Akbar <mrakbarasghari@gmail.com>
2026-01-25 17:24:23 +0330

    null checking

--- DNSLab.Web/AppSettings.cs
+++ DNSLab.Web/AppSettings.cs
@@ -6,7 +6,7 @@ public sealed class GlobalSettings
 {
     public const string ApplicationName = "DNSLab";
     public const bool RightToLeft = true;
-    public const string Version = "1.9.3.7";
+    public const string Version = "1.9.4.2";
 
 #if DEBUG
     //public const string APIBaseAddress = "https://localhost:7046/";
